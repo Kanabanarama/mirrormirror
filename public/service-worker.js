5fbdda605593e248b441832a3fb59416cf6b0ac5
Kana <kanabanarama@googlemail.com>
2020-09-18 14:54:18 +0200

    Add service workers

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,11 +1,44 @@
-self.addEventListener('install', function (event) {
-  console.log('service worker: cache data')
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('mirrormirror').then(function(cache)
+    {
+      return cache.addAll([
+        '/',
+        '/manifest.json',
+        '/index.htm',
+        '/js/main.js',
+        '/styles/main.css',
+        '/gfx/icon-192x192.png',
+        '/gfx/icon-maskable-192x192.png',
+        '/gfx/splash-screen-512x512.png'
+      ])
+    })
+  )
 })
 
-self.addEventListener('fetch', function (event) {
-  console.log('service worker: intercept request to provide cached data')
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open('mirrormirror')
+      .then((cache) => {
+        return cache.match(event.request)
+          .then((response) => {
+            return response || fetch(event.request)
+          })
+      })
+  )
 })
 
-self.addEventListener('activate', function (event) {
-  console.log('service worker: activate')
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+     caches.keys()
+       .then((keyList) => {
+         return Promise.all(keyList.map((key) => {
+           if (key !== 'mirrormirror') {
+             console.log('[ServiceWorker] Removing old cache', key)
+             return caches.delete(key)
+           }
+         }))
+       })
+       .then(() => self.clients.claim())
+   )
 })
